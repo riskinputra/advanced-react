36bf9f4c1b57b72c6f5269df182ee95f9e48f343
riskinputra <riski.n.putra@gmail.com>
2018-09-29 21:34:29 +0700

    parsing comment list

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import reduxPromise from 'redux-promise '
+import reduxPromise from 'redux-promise'
 import reducers from 'reducers'
 
 export default ({ children, initialState = {} }) => {
